5d5a2bece69f409248feaeecf49f38598c572d92
Denzell Ford <fdenzell@gmail.com>
2022-12-03 14:37:08 -0600

    Added binance testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,6 +47,16 @@ module.exports = {
                     ],
         chainId: 595
       },
+      bnb : {
+        url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+        accounts: ["46e0483c6cb7a3b10d643a4ecc643633b5c89f73756d7921511fa2ae3bb8040e",
+                   "60b34f5cf893cf0463ccaf27c8a4a91509fd79708195afb0909573d9fe6da4cf",
+                   "4a9241c5e34cbb3605e69fb781b7391d799de131cdc8ba0c133076789f4f8933",
+                   "e91a7857301672ec176f9f8588fb393b382d549605e4440765130709296ed40e"
+                    ],
+        chainId: 97
+
+      }
   },
   mocha: {
     timeout: 1200000
